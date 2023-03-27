4e494172950b1ffd92914e69755a88cb2f1c4e5e
AnthonyCouthures <anthony.couthures@gmail.com>
2023-03-27 17:59:03 +0200

    cleaning

--- data.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-CHINA = ['CHN']
-ASIA_EXC_CHINA = ['IND', 'JPN', 'IRN', 'SAU', 'KOR', 'IDN', 'THA', 'KAZ',  'MYS']
-NORTH_AMERICA = ['USA', 'CAN', 'MEX']
-EUROPE =  ['AUT', 'BEL', 'BGR', 'CYP', 'CZE', 'DEU', 'DNK', 'ESP', 'EST', 'FIN', 'FRA', 'GRC', 'HRV', 'HUN', 'IRL', 'ITA', 'LTU', 'LUX', 'LVA', 'MLT', 'NLD', 'POL', 'PRT', 'ROU', 'SVK', 'SVN', 'SWE']
